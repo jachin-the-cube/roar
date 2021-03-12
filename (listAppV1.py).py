278cacf87da13b32baa0ed7191e3032fd42cae1f
jachin-the-cube <79867029+jachin-the-cube@users.noreply.github.com>
2021-03-12 11:00:27 -0700

    Add files via upload

--- (listAppV1.py).py
+++ (listAppV1.py).py
@@ -15,20 +15,28 @@ def mainProgram():
    while True:
       print ("Hello there, lets work with list")
       print ("plese choose from the following options. ty[e the number of the choice")
-      choice = input("1. add to a list, 2. return a value at a list, or type three to quit   ")
+      choice = input("""1. add to a list,
+2. return a value at a list, or type
+3. random Serch 
+4. to quit   """)
       if choice == "1":
           addToList()
       elif choice == "2":
            indecValue()
       elif choice == "3":
-         break
+         randomSearch()
+         else:
+             break
         
 def addToList():
      print(" adding to a list great choice")
      newItem = input(" type an interger here!  ")
      myList.append(int( newitem))
      #  we need to think about errors 
-
+def randomSearch() :
+   print("OH im so RanDoM :)")
+   myList[random.randint(0,  len(myList)-1)])
+   
 
 def indexValues():
    print("at what index position do you want you want to search?")
